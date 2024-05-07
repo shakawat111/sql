c9aee2b6c2ee92b9c9a489c0b5d954646de6c707
Md.Shakawat Hasan <120039232+shakawat111@users.noreply.github.com>
2024-05-07 11:15:26 +0600

    Add files via upload

--- mysql.php
+++ mysql.php
@@ -2,6 +2,7 @@
     $server_name = "localhost:3307";
     $username = "root";
     $password = "";
+    $db_name = "myDB";
 
     $connection=mysqli_connect($server_name,$username,$password);
 
@@ -11,6 +12,29 @@
     else{
         echo "connection to MySQL server successfully...";
     }
+//  //create database
+//  $SQL="CREATE DATABASE myDB";
+    
+
+//  if(mysqli_query($connection,$SQL)){
+//      echo "Database created successfully";
+//  } else {
+//      echo "Error. creating database ";
+//  }
+
+    // create table query
+
+    mysqli_query($connection,"use myDB");
+
+    // $query = "create table users(id int auto_increment primary key,fname varchar(100) not null,lname varchar(100),email varchar (50) not null)";
+
+    // if(mysqli_query($connection,$query)){
+    //     echo "Table creation successfully";
+    // }else{
+    //     echo "Error table creation...";
+    // }
+
+    
 
     mysqli_close($connection);
 
